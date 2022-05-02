a99e8a00387588316705f100296594b7a2925c4a
rrondosa <1017.reyna@gmail.com>
2022-05-02 16:52:42 -0300

    Se cambia la multipplicacion por la suma

--- Calculadora.java
+++ Calculadora.java
@@ -8,8 +8,8 @@ public class Ahorcado {
 		Integer opcion;        
 		Integer resultado;
 		do{
-			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para "
-					+ "restar /n 3 para multiplicar /n 4 para dividir");
+			System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para "
+					+ "restar /n 3 para sumar  /n 4 para dividir");
 			 opcion = teclado.next();
 		}while(opcion < 1 && opcion > 4)
 			System.out.println("Ingrese el primer numero");
@@ -18,13 +18,15 @@ public class Ahorcado {
 		a = teclado.next();
 		switch (opcion) {
 		case opcion.equals(1):
-			resultado = a + b; 
+			resultado = a * b;
 			break;
 		case opcion.equals(2):
-			break;
 			resultado = a - b;
+			break;
+			
 		case opcion.equals(3):
-			resultado = a * b;
+			
+			resultado = a + b; 
 			break;
 		default:
 			resultado = a / b;
